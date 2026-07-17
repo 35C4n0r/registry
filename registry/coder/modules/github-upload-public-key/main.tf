41a77b1dba92b6ec15dcb50a6809fca81491391b
Jay Kumar <jay.kumar@coder.com>
2026-07-17 10:07:47 +0000

    refactor(github-upload-public-key): move to run.sh.tftpl, add key_name var, extract templatefile to locals
    - Rename run.sh to run.sh.tftpl to make Terraform template intent explicit
    - Extract templatefile() call from coder_script into a locals block
    - Add key_name variable for configurable SSH key title in GitHub
    - Default key title falls back to '<access URL> Workspaces' when unset
    
    Signed-off-by: Jay Kumar <jay.kumar@coder.com>

--- registry/coder/modules/github-upload-public-key/main.tf
+++ registry/coder/modules/github-upload-public-key/main.tf
@@ -26,17 +26,28 @@ variable "github_api_url" {
   default     = "https://api.github.com"
 }
 
+variable "key_name" {
+  description = "The title assigned to the uploaded SSH key in GitHub. Defaults to '<Coder access URL> Workspaces'."
+  type        = string
+  default     = null
+}
+
 data "coder_workspace" "me" {}
 data "coder_workspace_owner" "me" {}
 
-resource "coder_script" "github_upload_public_key" {
-  agent_id = var.agent_id
-  script = templatefile("${path.module}/run.sh", {
+locals {
+  script = templatefile("${path.module}/run.sh.tftpl", {
     CODER_OWNER_SESSION_TOKEN : data.coder_workspace_owner.me.session_token,
     CODER_ACCESS_URL : data.coder_workspace.me.access_url,
     CODER_EXTERNAL_AUTH_ID : var.external_auth_id,
     GITHUB_API_URL : var.github_api_url,
+    CODER_PUBLIC_KEY_NAME : var.key_name == null ? "" : var.key_name,
   })
+}
+
+resource "coder_script" "github_upload_public_key" {
+  agent_id     = var.agent_id
+  script       = local.script
   display_name = "Github Upload Public Key"
   icon         = "/icon/github.svg"
   run_on_start = true
